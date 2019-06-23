3d0432e4ad989bbd065690b4bba7e16e6a385aa0
tailuge <tailuge@gmail.com>
2019-06-23 20:04:31 +0000

    fix imports

--- src/view/camera.ts
+++ src/view/camera.ts
@@ -1,45 +1,45 @@
-import { Table } from "../model/table"
-import { up, zero } from "../utils/utils"
+import { PerspectiveCamera, Vector3 } from "three";
+import { Table } from "../model/table";
+import { up, zero } from "../utils/utils";
 
-import * as THREE from "three"
 
 export class Camera {
-  constructor(table: Table, aspectRatio) {
-    this.table = table
-    this.camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000)
-  }
+    constructor(table: Table, aspectRatio) {
+        this.table = table
+        this.camera = new PerspectiveCamera(75, aspectRatio, 0.1, 1000)
+    }
 
-  camera
-  table: Table
-  mode = this.topView
+    camera: PerspectiveCamera
+    table: Table
+    mode = this.topView
 
-  private topViewPoint = new THREE.Vector3(0, -0.1, 17)
+    private topViewPoint = new Vector3(0, -0.1, 17)
 
-  update(t) {
-    this.t += t
-    this.mode()
-  }
+    update(t) {
+        this.t += t
+        this.mode()
+    }
 
-  t = 0
-  topView() {
-    this.camera.position.lerp(this.topViewPoint, 0.1)
-    this.camera.up = up
-    this.camera.lookAt(zero)
-  }
+    t = 0
+    topView() {
+        this.camera.position.lerp(this.topViewPoint, 0.1)
+        this.camera.up = up
+        this.camera.lookAt(zero)
+    }
 
-  aimView() {
-    this.camera.position.lerp(
-      this.table.balls[0].pos.clone().addScaledVector(this.table.cue.aim.dir, -9),
-      0.07
-    )
-    this.camera.position.z = 2.7
-    this.camera.up = up
-    this.camera.lookAt(this.table.balls[0].pos)
-  }
+    aimView() {
+        this.camera.position.lerp(
+            this.table.balls[0].pos.clone().addScaledVector(this.table.cue.aim.dir, -9),
+            0.07
+        )
+        this.camera.position.z = 2.7
+        this.camera.up = up
+        this.camera.lookAt(this.table.balls[0].pos)
+    }
 
-  afterHitView() {
-    this.camera.position.addScaledVector(up, 0.01)
-    this.camera.up = up
-    this.camera.lookAt(this.table.balls[0].pos)
-  }
+    afterHitView() {
+        this.camera.position.addScaledVector(up, 0.01)
+        this.camera.up = up
+        this.camera.lookAt(this.table.balls[0].pos)
+    }
 }
