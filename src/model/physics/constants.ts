e891294b6e760fc77027f0c92def6e0ff59c489c
tailuge <tailuge@gmail.com>
2019-08-05 20:36:02 +0000

    update

--- src/model/physics/constants.ts
+++ src/model/physics/constants.ts
@@ -1,6 +1,6 @@
 export const mu = 0.006
 export const g = 9.8
-export const rho = 0.4
+export const rho = 0.3
 export const m = 1
 export const Mz = ((mu * m * g * 2) / 3) * rho
 export const Mxy = (7 / (5 * Math.sqrt(2))) * mu * m * g
