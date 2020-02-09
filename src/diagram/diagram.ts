55632c6c4eecb0bcbb9cfd88d87000916f6c10fc
tailuge <tailuge@gmail.com>
2020-02-09 08:54:35 +0000

    animate diagram

--- src/diagram/diagram.ts
+++ src/diagram/diagram.ts
@@ -5,12 +5,15 @@ export class Diagram {
   table: Table
   diagram: CanvasRenderingContext2D
   control: HTMLElement
+  last = performance.now()
+  readonly step = 0.01
 
   constructor(state, diagram, control) {
     this.table = Table.fromSerialised(state)
     this.diagram = diagram
     this.control = control
     this.drawTable()
+    this.animate(this.last)
   }
 
   scale() {
@@ -34,16 +37,33 @@ export class Diagram {
       TableGeometry.X * 2,
       TableGeometry.Y * 2
     )
-
-    this.drawBalls()
   }
 
-  drawBalls() {
-    this.diagram.fillStyle = "#aa0000"
+  drawBalls(style) {
+    this.diagram.fillStyle = style
     this.table.balls.forEach(ball => {
       this.diagram.beginPath()
-      this.diagram.ellipse(ball.pos.x, ball.pos.y, 0.5, 0.5, 0, 0, 7)
+      this.diagram.ellipse(ball.pos.x, ball.pos.y, 0.5, 0.5, 0, 0, Math.PI * 2)
       this.diagram.fill()
     })
+  }
+
+  advance(elapsed) {
+    const steps = Math.max(15, Math.floor(elapsed / this.step))
+    for (var i = 0; i < steps; i++) {
+      this.table.advance(this.step)
+    }
+  }
+
+  animate(timestamp): void {
+    this.drawBalls("#0a5c5c")
+    this.advance((timestamp - this.last) / 1000)
+    this.last = timestamp
+    this.drawBalls("#aaaaaa")
+    if (!this.table.allStationary()) {
+      requestAnimationFrame(t => {
+        this.animate(t)
+      })
+    }
   }
 }
