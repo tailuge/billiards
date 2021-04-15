ac1f8c40db061f4e09f563a08bcaae290fa791ba
tailuge <tailuge@gmail.com>
2021-04-15 19:29:59 +0000

    simpler inputs

--- src/view/aiminputs.ts
+++ src/view/aiminputs.ts
@@ -14,6 +14,7 @@ export class AimInputs {
     this.cueBallElement = document.getElementById("cueBall")
     this.cueTipElement = document.getElementById("cueTip")
     this.cueBallElement?.addEventListener("pointermove", this.mousemove)
+    this.cueBallElement?.addEventListener("click", this.click)
 
     this.cuePowerElement = document.getElementById("cuePower")
     document.addEventListener("wheel", this.mousewheel)
@@ -23,19 +24,27 @@ export class AimInputs {
     document.addEventListener("dblclick", this.hit)
   }
 
+  click = (e) => {
+    this.adjustSpin(e)
+  }
+
   mousemove = (e) => {
     if (e.buttons === 1) {
-      const ballDiameter = this.cueBallElement.offsetWidth
-      const tipRadius = this.cueTipElement.offsetWidth / 2
-      let clamped = this.container.table.cue.setSpin(
-        -(e.offsetX - ballDiameter / 2) / ballDiameter,
-        -(e.offsetY - ballDiameter / 2) / ballDiameter
-      )
-      this.cueTipElement.style.left =
-        (-clamped.x + 0.5) * ballDiameter - tipRadius + "px"
-      this.cueTipElement.style.top =
-        (-clamped.y + 0.5) * ballDiameter - tipRadius + "px"
+      this.adjustSpin(e)
     }
+  }
+
+  adjustSpin(e) {
+    const ballDiameter = this.cueBallElement.offsetWidth
+    const tipRadius = this.cueTipElement.offsetWidth / 2
+    let clamped = this.container.table.cue.setSpin(
+      -(e.offsetX - ballDiameter / 2) / ballDiameter,
+      -(e.offsetY - ballDiameter / 2) / ballDiameter
+    )
+    this.cueTipElement.style.left =
+      (-clamped.x + 0.5) * ballDiameter - tipRadius + "px"
+    this.cueTipElement.style.top =
+      (-clamped.y + 0.5) * ballDiameter - tipRadius + "px"
   }
 
   hit = (_) => {
