fdc434c3413e10668495453af62e401e94aaeac0
tailuge <tailuge@gmail.com>
2021-09-05 15:38:25 +0000

    websocket

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,37 @@
-import {Server} from 'ws';
-var port = 8888
-// initialize the WebSocket server instance
-const server = new Server({ port: port });
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8888 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+    ws.send('something');
+  });
+
+});
 
 
-console.log('Server is running on port', port, server);
+console.log('WebSocketServer is running');
+
+/*
+server:
+
+tsc src/server/server.ts 
+node src/server/server.js 
+
+client:
+
+echo $(gp url 8888)
+
+use url in:
+
+var ws = new WebSocket("wss://8888-beige-bobcat-bxbv4rs0.ws-eu16.gitpod.io/ws");
+ws.onclose = function() { console.log("close"); };
+ws.onerror = function() { console.log("error"); };
+ws.onmessage = function(e) { console.log("received:",e); };
+ws.onopen = function() { console.log("open"); };
+
+ws.send("hi")
+ws.close()
+
+*/
