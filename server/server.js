2b0ca23f23ea35ff717752be6b2c7d611165597b
tailuge <tailuge@gmail.com>
2021-09-19 17:59:08 +0000

    heroku

--- server/server.js
+++ server/server.js
@@ -5,28 +5,48 @@ console.log(`Starting websocketserver on port ${port}`);
 
 const wss = new WebSocket.Server({ port: port });
 
+const start=`{"type":"BEGIN"}`;
+const clientToRoom = [];
+const rooms = [[]];
+var pendingRoom = 0;
+var idFountain = 3000;
 wss.on("connection", function connection(ws) {
-  console.log("connection");
-  if (wss.clients.size == 2) {
-    console.log("Two connections, sending start");
-    const start=`{"type":"BEGIN"}`;
-    console.log(start);
-    ws.send(start);
-  }
-  ws.on("message", function incoming(message) {
-    console.log("received: %s", message);
-    sendToOther(ws, message.toString());
-  })
-})
 
-function sendToOther(ws, data) {
-  console.log("Send to others");
-  wss.clients.forEach((client) => {
-    if (client !== ws) {
+  ws.id = idFountain++;
+  rooms[pendingRoom].push(ws);
+  clientToRoom[ws.id] = pendingRoom;
+  console.log(`Room ${pendingRoom} has ${rooms[pendingRoom].length} partcipants`);
+  
+  if (rooms[pendingRoom].length == 2) {
+
+    // create pair and mkae new pending room
+    console.log(`Room ${pendingRoom} has ${rooms[pendingRoom].length} partcipants`);
+    pendingRoom++;
+    rooms[pendingRoom] = [];
+
+    // send begin event
+    console.log("Sending begin event");
+    ws.send(start);
+  } 
+
+
+  ws.on("message", function incoming(message) {
+    const m = JSON.parse(message);
+    console.log(`received: ${m.type} from ${ws.id}`);
+    sendToOthersInRoom(ws, message.toString());
+  });
+});
+
+function sendToOthersInRoom(ws, data) {
+  const room = rooms[clientToRoom[ws.id]];
+  room.forEach((client) => {
+    if (client.id !== ws.id) {
+      console.log(`Sending message in room ${clientToRoom[ws.id]} to ${client.id}`);
       client.send(data);
     }
-  });
+  })
 }
+
 
 console.log("WebsocketServer started")
 console.log("https://tailuge.github.io/billiards/dist/?websocketserver=wss://billiardsserver.herokuapp.com/ws")
