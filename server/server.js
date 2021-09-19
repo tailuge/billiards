7c7b637d1f183da2d80348dba39d6f34ffe63c38
tailuge <tailuge@gmail.com>
2021-09-19 10:25:49 +0000

    heroku

--- server/server.js
+++ server/server.js
@@ -6,10 +6,12 @@ console.log(`Starting websocketserver on port ${port}`);
 const wss = new WebSocket.Server({ port: port });
 
 wss.on("connection", function connection(ws) {
+  console.log("connection");
   if (wss.clients.size == 2) {
     console.log("Two connections, sending start");
-    console.log(EventUtil.serialise(new BeginEvent()));
-    ws.send(EventUtil.serialise(new BeginEvent()));
+    const start=`{"type":"BEGIN"}`;
+    console.log(start);
+    ws.send(start);
   }
   ws.on("message", function incoming(message) {
     console.log("received: %s", message);
@@ -18,6 +20,7 @@ wss.on("connection", function connection(ws) {
 })
 
 function sendToOther(ws, data) {
+  console.log("Send to others");
   wss.clients.forEach((client) => {
     if (client !== ws) {
       client.send(data);
@@ -27,4 +30,4 @@ function sendToOther(ws, data) {
 
 console.log("WebsocketServer started")
 console.log("https://tailuge.github.io/billiards/dist/?websocketserver=wss://billiardsserver.herokuapp.com/ws")
-
+console.log("https://dashboard.heroku.com/apps/billiardsserver/logs")
