fbfc4473dcf0bde7f9299ab85e6aece4c85e2873
tailuge <tailuge@gmail.com>
2023-08-10 18:22:52 +0000

    cushion diagrams

--- src/diagrams.ts
+++ src/diagrams.ts
@@ -1,6 +1,14 @@
 import { Diagram } from "./diagram/diagram"
 import { Chart } from "chart.js/auto"
-import { Pze, Pzs, c0, s0, muCushion } from "./model/physics/physics"
+import {
+  Pze,
+  Pzs,
+  c0,
+  s0,
+  muCushion,
+  rotateApplyUnrotate,
+  isGripCushion,
+} from "./model/physics/physics"
 import { Vector3 } from "three"
 
 console.log("Diagrams")
@@ -22,6 +30,10 @@ plot1()
 plot2()
 plot3()
 plot4()
+
+vectorPlot("vectorPlot1", 20, 80, 20, new Vector3(0, 0, 0))
+vectorPlot("vectorPlot2", 20, 80, 20, new Vector3(0, 0, -3))
+vectorPlot("vectorPlot3", 20, 80, 20, new Vector3(0, 0, 3))
 
 function plot1() {
   const x: number[] = []
@@ -70,14 +82,14 @@ function plot3() {
 
 function plot4() {
   const x: number[] = []
-  const yDataset = dataset()
+  const yDataset = dataset().slice(1)
+  yDataset[0].label = "mu"
   for (let i = -80; i <= 80; i += 10) {
     x.push(i)
     const rad = (i * Math.PI) / 180
     const v = new Vector3(Math.cos(rad), Math.sin(rad), 0)
     const mu = muCushion(v)
     yDataset[0].data.push(mu)
-    yDataset[1].data.push(mu)
   }
   plotOnCanvas(
     "plot4",
@@ -146,4 +158,49 @@ function elt(diagram, id) {
     throw new Error("Element not found " + selector)
   }
   return e
+}
+
+function vectorPlot(id, angleStart, angleEnd, angleStep, w) {
+  const canvas = document.getElementById(id) as HTMLCanvasElement
+  const context = canvas.getContext("2d")!
+  const endx = 100
+  const endy = 100
+  const dv = new Vector3()
+  const dw = new Vector3()
+  const s = 75
+  for (let i = angleStart; i <= angleEnd; i += angleStep) {
+    const rad = (i * Math.PI) / 180
+    const v = new Vector3(Math.cos(rad), Math.sin(rad), 0)
+    const lineDash = isGripCushion(v, w) ? [] : [2, 2]
+    context.setLineDash(lineDash)
+    context.strokeStyle = "blue"
+    drawArrow(context, endx - v.x * s, endy - v.y * s, endx, endy)
+    rotateApplyUnrotate(0, v, w, dv, dw)
+    v.add(dv)
+    context.strokeStyle = "red"
+    drawArrow(context, endx, endy, endx + v.x * s, endy + v.y * s)
+  }
+}
+
+function drawArrow(context, x1, y1, x2, y2, t = 0.9) {
+  const arrow = {
+    dx: x2 - x1,
+    dy: y2 - y1,
+  }
+  const middle = {
+    x: arrow.dx * t + x1,
+    y: arrow.dy * t + y1,
+  }
+  const tip = {
+    dx: x2 - middle.x,
+    dy: y2 - middle.y,
+  }
+  context.beginPath()
+  context.moveTo(x1, y1)
+  context.lineTo(middle.x, middle.y)
+  context.moveTo(middle.x + 0.5 * tip.dy, middle.y - 0.5 * tip.dx)
+  context.lineTo(middle.x - 0.5 * tip.dy, middle.y + 0.5 * tip.dx)
+  context.lineTo(x2, y2)
+  context.closePath()
+  context.stroke()
 }
