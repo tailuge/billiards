6f011e5b6b518c57a398368f874ca5a3cf4deaa2
tailuge <tailuge@gmail.com>
2023-08-22 20:56:09 +0100

    extract geometry

--- src/view/cue.ts
+++ src/view/cue.ts
@@ -1,19 +1,12 @@
 import { TableGeometry } from "../view/tablegeometry"
 import { Table } from "../model/table"
-import { up, upCross, unitAtAngle } from "../utils/utils"
+import { upCross, unitAtAngle } from "../utils/utils"
 import { AimEvent } from "../events/aimevent"
 import { AimInputs } from "./aiminputs"
 import { Ball, State } from "../model/ball"
-import {
-  Matrix4,
-  Mesh,
-  CylinderGeometry,
-  MeshPhongMaterial,
-  Raycaster,
-  Vector3,
-  MathUtils,
-} from "three"
 import { cueToSpin } from "../model/physics/physics"
+import { CueMesh } from "./cuemesh"
+import { Mesh, Raycaster, Vector3, MathUtils } from "three"
 
 export class Cue {
   mesh: Mesh
@@ -26,49 +19,9 @@ export class Cue {
 
   length = TableGeometry.tableX * 1
 
-  private static material = new MeshPhongMaterial({
-    color: 0x885577,
-    wireframe: false,
-    flatShading: false,
-  })
-
-  private static helpermaterial = new MeshPhongMaterial({
-    color: 0x885577,
-    wireframe: false,
-    flatShading: true,
-    transparent: true,
-    opacity: 0.3,
-  })
-
   constructor() {
-    this.initialise(0.05, 0.15, this.length)
-  }
-
-  private createHelper() {
-    const geometry = new CylinderGeometry(0.5, 0.5, 30, 12)
-    this.helperMesh = new Mesh(geometry, Cue.helpermaterial)
-    this.helperMesh.geometry
-      .applyMatrix4(new Matrix4().identity().makeRotationAxis(up, -Math.PI / 2))
-      .applyMatrix4(new Matrix4().identity().makeTranslation(15, 0, 0))
-    this.helperMesh.visible = false
-  }
-
-  private initialise(tip, but, length) {
-    const geometry = new CylinderGeometry(tip, but, length, 11)
-    this.mesh = new Mesh(geometry, Cue.material)
-    this.mesh.castShadow = false
-    this.mesh.geometry
-      .applyMatrix4(
-        new Matrix4()
-          .identity()
-          .makeRotationAxis(new Vector3(1.0, 0.0, 0.0), -0.1)
-      )
-      .applyMatrix4(new Matrix4().identity().makeRotationAxis(up, -Math.PI / 2))
-      .applyMatrix4(
-        new Matrix4().identity().makeTranslation(-length / 2 - 0.5, 0, 1)
-      )
-    this.mesh.rotation.z = this.aim.angle
-    this.createHelper()
+    this.mesh = CueMesh.createCue(0.05, 0.15, this.length)
+    this.helperMesh = CueMesh.createHelper()
   }
 
   rotateAim(angle) {
---
+++ src/view/cuemesh.ts
@@ -0,0 +1,54 @@
+import { TableGeometry } from "../view/tablegeometry"
+import { up } from "../utils/utils"
+import {
+  Matrix4,
+  Mesh,
+  CylinderGeometry,
+  MeshPhongMaterial,
+  Vector3,
+} from "three"
+
+export class CueMesh {
+  length = TableGeometry.tableX * 1
+
+  private static readonly material = new MeshPhongMaterial({
+    color: 0x885577,
+    wireframe: false,
+    flatShading: false,
+  })
+
+  private static readonly helpermaterial = new MeshPhongMaterial({
+    color: 0x885577,
+    wireframe: false,
+    flatShading: true,
+    transparent: true,
+    opacity: 0.3,
+  })
+
+  static createHelper() {
+    const geometry = new CylinderGeometry(0.5, 0.5, 30, 12)
+    const mesh = new Mesh(geometry, CueMesh.helpermaterial)
+    mesh.geometry
+      .applyMatrix4(new Matrix4().identity().makeRotationAxis(up, -Math.PI / 2))
+      .applyMatrix4(new Matrix4().identity().makeTranslation(15, 0, 0))
+    mesh.visible = false
+    return mesh
+  }
+
+  static createCue(tip, but, length) {
+    const geometry = new CylinderGeometry(tip, but, length, 11)
+    const mesh = new Mesh(geometry, CueMesh.material)
+    mesh.castShadow = false
+    mesh.geometry
+      .applyMatrix4(
+        new Matrix4()
+          .identity()
+          .makeRotationAxis(new Vector3(1.0, 0.0, 0.0), -0.1)
+      )
+      .applyMatrix4(new Matrix4().identity().makeRotationAxis(up, -Math.PI / 2))
+      .applyMatrix4(
+        new Matrix4().identity().makeTranslation(-length / 2 - 0.5, 0, 1)
+      )
+    return mesh
+  }
+}
