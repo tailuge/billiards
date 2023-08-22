63ec06f98a04d41f0dd468f2e4e8934c447f759a
tailuge <tailuge@gmail.com>
2023-08-22 16:41:53 +0100

    diagram

--- src/diagram/cushionplot.ts
+++ src/diagram/cushionplot.ts
@@ -7,6 +7,7 @@ export class CushionPlot {
   readonly endx = 100
   readonly endy = 100
   readonly scale = 75
+  readonly r = 20
   readonly dv = new Vector3()
   readonly dw = new Vector3()
 
@@ -14,6 +15,23 @@ export class CushionPlot {
     div.firstElementChild!.innerHTML = title
     this.canvas = div.lastElementChild as HTMLCanvasElement
     this.context = this.canvas.getContext("2d") as CanvasRenderingContext2D
+  }
+
+  drawBall() {
+    this.context.beginPath()
+    this.context.strokeStyle = "lightgray"
+    this.context.fillStyle = "beige"
+    this.context.arc(this.endx, this.endy, this.r, 0, 2 * Math.PI, false)
+    this.context.fill()
+    this.context.stroke()
+  }
+
+  drawCushion() {
+    const gradient = this.context.createLinearGradient(10, 90, 200, 90)
+    gradient.addColorStop(0, "lightgray")
+    gradient.addColorStop(0.75, "white")
+    this.context.fillStyle = gradient
+    this.context.fillRect(this.endx + this.r, 10, 200, 250)
   }
 
   plot(
@@ -24,12 +42,15 @@ export class CushionPlot {
     fw: (a: number) => Vector3
   ) {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    this.drawCushion()
+    this.drawBall()
     for (let i = angleStart; i <= angleEnd; i += angleStep) {
       const v: Vector3 = fv(i)
       const w: Vector3 = fw(i)
       const lineDash = isGripCushion(v, w) ? [] : [2, 2]
       this.context.setLineDash(lineDash)
-      this.context.strokeStyle = "blue"
+      const hue = ((i + 360) * 101) % 360
+      this.context.strokeStyle = `hsl(${hue},50%,50%)`
       this.drawArrow(
         this.endx - v.x * this.scale,
         this.endy - v.y * this.scale,
@@ -38,7 +59,6 @@ export class CushionPlot {
       )
       rotateApplyUnrotate(0, v, w, this.dv, this.dw)
       v.add(this.dv)
-      this.context.strokeStyle = "red"
       this.drawArrow(
         this.endx,
         this.endy,
