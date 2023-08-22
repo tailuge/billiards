0e190d0f9e32ac587dc578752365172ec37ba96a
tailuge <tailuge@gmail.com>
2023-08-22 09:40:55 +0100

    prettify

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,52 @@
-const path = require('path');
-const TerserPlugin = require("terser-webpack-plugin");
-let packagedeps = require('./package.json');
+const path = require("path")
+const TerserPlugin = require("terser-webpack-plugin")
+let packagedeps = require("./package.json")
 module.exports = {
-    entry: {
-        vendordiagram: Object.keys(packagedeps.dependencies),
-        vendor: ["three","interactjs"],
-        index: { dependOn: 'vendor', import: './src/index.ts' },
-        diagram: { dependOn: 'vendordiagram', import: './src/diagrams.ts' }
+  entry: {
+    vendordiagram: Object.keys(packagedeps.dependencies),
+    vendor: ["three", "interactjs"],
+    index: { dependOn: "vendor", import: "./src/index.ts" },
+    diagram: { dependOn: "vendordiagram", import: "./src/diagrams.ts" },
+  },
+  module: {
+    rules: [
+      {
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
     },
-    module: {
-        rules: [
-            {
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
+    allowedHosts: [".gitpod.io"],
+    host: "0.0.0.0",
+    compress: true,
+    port: 8080,
+    client: {
+      progress: true,
     },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
-          },
-        allowedHosts: ['.gitpod.io'],
-        host: '0.0.0.0',
-        compress: true,
-        port: 8080,
-        client: {
-            progress: true,
-        }
-    },
-    performance: { hints: false },
-    mode: 'production',
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                extractComments: false,
-                terserOptions: {
-                    safari10: true   
-                }
-            }),
-        ],
-        usedExports: true,
-        moduleIds: 'named'
-    },
-    stats: {
-        errorDetails: true
-    }
-};
+  },
+  performance: { hints: false },
+  mode: "production",
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          safari10: true,
+        },
+      }),
+    ],
+    usedExports: true,
+    moduleIds: "named",
+  },
+  stats: {
+    errorDetails: true,
+  },
+}
