7c67697eaf89820f8b00f0c475d39a68f42dff07
tailuge <tailuge@gmail.com>
2023-09-11 12:45:20 +0100

    threecushion rules

--- src/rules/threecushion.ts
+++ src/rules/threecushion.ts
@@ -1,13 +1,8 @@
 import { Container } from "../container/container"
 import { Aim } from "../controller/aim"
 import { Controller } from "../controller/controller"
-import { PlaceBall } from "../controller/placeball"
-import { WatchAim } from "../controller/watchaim"
-import { PlaceBallEvent } from "../events/placeballevent"
-import { WatchEvent } from "../events/watchevent"
 import { Outcome } from "../model/outcome"
 import { Rack } from "../utils/rack"
-import { zero } from "../utils/utils"
 import { Rules } from "./rules"
 
 export class ThreeCushion implements Rules {
