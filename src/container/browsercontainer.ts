a5c0a5e93a6800571f1dfd013a1f3d263b1cd972
tailuge <tailuge@gmail.com>
2023-09-11 08:44:43 +0100

    container reorg

---
+++ src/container/browsercontainer.ts
@@ -0,0 +1,112 @@
+import { Container } from "./container"
+import { Keyboard } from "../events/keyboard"
+import { EventUtil } from "../events/eventutil"
+import { EventType } from "../events/eventtype"
+import { BreakEvent } from "../events/breakevent"
+import { SocketConnection } from "../events/socketconnection"
+import { ChatEvent } from "../events/chatevent"
+
+/**
+ * Integrate game container into HTML page
+ */
+export class BrowserContainer {
+  container: Container
+  canvas3d
+  tablename
+  wss
+  playername: string
+  replay: string | null
+  sc: SocketConnection | null = null
+  breakState = {
+    init: null,
+    shots: Array<string>(),
+  }
+
+  constructor(playername, tablename, replay, wss, canvas3d) {
+    this.playername = playername
+    this.tablename = tablename
+    this.replay = replay
+    this.wss = wss
+    this.canvas3d = canvas3d
+  }
+
+  start() {
+    const keyboard = new Keyboard(this.canvas3d)
+    this.container = new Container(
+      this.canvas3d,
+      console.log,
+      keyboard,
+      () => {
+        this.onAssetsReady()
+      },
+      this.playername
+    )
+    this.container.broadcast = (e) => {
+      this.recordingBroadcast(e)
+    }
+    if (this.wss) {
+      this.sc = new SocketConnection(
+        `${this.wss}?name=${this.playername}&table=${this.tablename}`
+      )
+      this.sc.eventHandler = (e) => {
+        this.netEvent(e)
+      }
+      this.container.isSinglePlayer = false
+    }
+  }
+
+  onAssetsReady() {
+    console.log(`${this.playername} ready`)
+
+    if (this.replay) {
+      this.container.table.cue.aimInputs.setButtonText("↻")
+      this.container.table.cue.aimInputs.cueHitElement?.addEventListener(
+        "click",
+        () => {
+          location.reload()
+        }
+      )
+      this.breakState = JSON.parse(decodeURI(this.replay))
+      this.container.eventQueue.push(
+        new BreakEvent(this.breakState.init, this.breakState.shots)
+      )
+    }
+
+    if (!this.sc) {
+      this.container.eventQueue.push(new BreakEvent())
+    }
+
+    // trigger animation loops
+    this.container.animate(performance.now())
+  }
+
+  netEvent(e: string) {
+    const event = EventUtil.fromSerialised(e)
+    console.log(`${this.playername} received ${event.type}`)
+    this.container.eventQueue.push(event)
+  }
+
+  recordingBroadcast(e: string) {
+    const event = EventUtil.fromSerialised(e)
+    if (event.type === EventType.HIT) {
+      this.shotReplayLink()
+    }
+    this.sc?.send(e)
+  }
+
+  shotReplayLink() {
+    const url = window.location.href.split("?")[0]
+    const prefix = `${url}?state=`
+    const serialisedShot = JSON.stringify(
+      this.container.recoder.replayLastShot()
+    )
+    const shotUri = encodeURI(`${prefix}${serialisedShot}`)
+    const serialisedBreak = JSON.stringify(this.container.recoder.replayGame())
+    const breakUri = encodeURI(`${prefix}${serialisedBreak}`)
+    const breakLink = `<a class="pill" target="_blank" href="${breakUri}">break ⚆...</a>`
+    const shotLink = `<a class="pill" target="_blank" href="${shotUri}">shot ⚆</a>`
+    this.container.eventQueue.push(
+      new ChatEvent("share", `${shotLink} ${breakLink}`)
+    )
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,89 +1,20 @@
-import { Container } from "./container/container"
-import { Keyboard } from "./events/keyboard"
-import { EventUtil } from "./events/eventutil"
-import { EventType } from "./events/eventtype"
-import { BreakEvent } from "./events/breakevent"
-import { SocketConnection } from "./events/socketconnection"
-import { ChatEvent } from "./events/chatevent"
-
-let name: string
-let replay: string | null
-let sc: SocketConnection | null = null
-let container: Container
-let breakState = {
-  init: null,
-  shots: Array<string>(),
-}
+import { BrowserContainer } from "./container/browsercontainer"
 
 initialise()
 
 function initialise() {
+  const canvas3d = document.getElementById("viewP1") as HTMLCanvasElement
   const params = new URLSearchParams(location.search)
   const wss = params.get("websocketserver")
-  const table = params.get("table") ?? "default"
-  name = params.get("name") ?? ""
-  replay = params.get("state")
-  const canvas3d = document.getElementById("viewP1") as HTMLCanvasElement
-  const keyboard = new Keyboard(canvas3d)
-  container = new Container(
-    canvas3d,
-    console.log,
-    keyboard,
-    onAssetsReady,
-    name
+  const tablename = params.get("table") ?? "default"
+  const playername = params.get("name") ?? ""
+  const replay = params.get("state")
+  const browserContainer = new BrowserContainer(
+    playername,
+    tablename,
+    replay,
+    wss,
+    canvas3d
   )
-  container.broadcast = recordingBroadcast
-  if (wss) {
-    sc = new SocketConnection(`${wss}?name=${name}&table=${table}`)
-    sc.eventHandler = netEvent
-    container.isSinglePlayer = false
-  }
-}
-
-function onAssetsReady() {
-  console.log(`${name} ready`)
-
-  if (replay) {
-    container.table.cue.aimInputs.setButtonText("↻")
-    container.table.cue.aimInputs.cueHitElement?.addEventListener(
-      "click",
-      () => {
-        location.reload()
-      }
-    )
-    breakState = JSON.parse(decodeURI(replay))
-    container.eventQueue.push(new BreakEvent(breakState.init, breakState.shots))
-  }
-
-  if (!sc) {
-    container.eventQueue.push(new BreakEvent())
-  }
-
-  // trigger animation loops
-  container.animate(performance.now())
-}
-
-function netEvent(e: string) {
-  const event = EventUtil.fromSerialised(e)
-  console.log(`${name} received ${event.type}`)
-  container.eventQueue.push(event)
-}
-
-function recordingBroadcast(e: string) {
-  const event = EventUtil.fromSerialised(e)
-  if (event.type === EventType.HIT) {
-    recordShot()
-  }
-  sc?.send(e)
-}
-
-function recordShot() {
-  const prefix = `${window.location}?state=`
-  const serialisedShot = JSON.stringify(container.recoder.replayLastShot())
-  const shotUri = encodeURI(`${prefix}${serialisedShot}`)
-  const serialisedBreak = JSON.stringify(container.recoder.replayGame())
-  const breakUri = encodeURI(`${prefix}${serialisedBreak}`)
-  const breakLink = `<a class="pill" target="_blank" href="${breakUri}">break ⚆...</a>`
-  const shotLink = `<a class="pill" target="_blank" href="${shotUri}">shot ⚆</a>`
-  container.eventQueue.push(new ChatEvent("share", `${shotLink} ${breakLink}`))
+  browserContainer.start()
 }
