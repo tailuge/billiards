7cd8fa68c4e223b0d4e2a00d5d922aadb9d472bb
tailuge <tailuge@gmail.com>
2023-10-20 20:48:29 +0100

    sonar

--- test/rules/fourteenone.spec.ts
+++ test/rules/fourteenone.spec.ts
@@ -1,13 +1,7 @@
 import "mocha"
 import { expect } from "chai"
 import { Container } from "../../src/container/container"
-import { Aim } from "../../src/controller/aim"
-import { PlayShot } from "../../src/controller/playshot"
-import { StationaryEvent } from "../../src/events/stationaryevent"
 import { GameEvent } from "../../src/events/gameevent"
-import { Outcome } from "../../src/model/outcome"
-import { RuleFactory } from "../../src/controller/rules/rulefactory"
-import { WatchAim } from "../../src/controller/watchaim"
 import { initDom } from "../view/dom"
 
 initDom()
