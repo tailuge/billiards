afc22a0c1b2497b699f4881075f288a158e44089
tailuge <tailuge@gmail.com>
2024-12-28 14:21:31 +0000

    upgrade chai deepseek

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   rootDir: "../",
   preset: "ts-jest",
-  transformIgnorePatterns: ["node_modules/(?!jsoncrush)"],
+  transformIgnorePatterns: [
+    // Exclude all node_modules except for chai and jsoncrush
+    "node_modules/(?!(chai|jsoncrush))",
+  ],
   transform: {
+    // Use SWC for transforming both JavaScript and TypeScript files
     "^.+\\.(t|j)sx?$": [
       "@swc/jest",
       {
@@ -12,6 +16,12 @@ module.exports = {
             mangle: false,
             format: { comments: "all" },
           },
+          // Enable ESM support in SWC
+          parser: {
+            syntax: "typescript",
+            tsx: true,
+          },
+          target: "es2020",
         },
       },
     ],
@@ -30,5 +40,12 @@ module.exports = {
     ".*GLTFExporter": "<rootDir>/test/mocks/gltfexporter.ts",
     ".*GLTFLoader": "<rootDir>/test/mocks/gltfloader.ts",
     ".*/sound": "<rootDir>/test/mocks/mocksound.ts",
+  },
+  // Enable ESM support in Jest
+  extensionsToTreatAsEsm: [".ts"],
+  globals: {
+    "ts-jest": {
+      useESM: true, // Enable ESM support in ts-jest
+    },
   },
 }
