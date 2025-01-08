2d4849f590b803557a82248005beb103e5650843
tailuge <tailuge@gmail.com>
2025-01-08 17:15:09 +0000

    spectate

--- src/controller/init.ts
+++ src/controller/init.ts
@@ -8,6 +8,7 @@ import { PlaceBall } from "./placeball"
 import { Replay } from "./replay"
 import { Session } from "../network/client/session"
 import { Spectate } from "./spectate"
+import { NchanMessageRelay } from "../network/client/nchanmessagerelay"
 
 /**
  * Initial state of controller.
@@ -17,7 +18,7 @@ import { Spectate } from "./spectate"
 export class Init extends ControllerBase {
   override handleBegin(_: BeginEvent): Controller {
     if (Session.isSpectator()) {
-      return new Spectate(this.container)
+      return new Spectate(this.container, new NchanMessageRelay(), Session.getInstance().tableId)
     }
 
     this.container.chat.showMessage("Start")
--- src/controller/spectate.ts
+++ src/controller/spectate.ts
@@ -1,11 +1,30 @@
 import { ControllerBase } from "./controllerbase"
 import { Controller, Input } from "./controller"
+import { MessageRelay } from "../network/client/messagerelay"
+import { ChatEvent } from "../events/chatevent"
 
 export class Spectate extends ControllerBase {
-  constructor(container) {
+
+  messageRelay: MessageRelay
+  tableId: string
+  messages: string[] = []
+  constructor(container, messageRelay, tableId) {
     super(container)
+    this.messageRelay = messageRelay
+    this.tableId = tableId
+    this.messageRelay.subscribe(this.tableId, (message) => {
+      console.log(message)
+      this.messages.push(message)
+      this.container.eventQueue.push(new ChatEvent("nchan", "."))
+    })
     console.log("Spectate")
   }
+
+  override handleChat(chatevent: ChatEvent): Controller {
+    console.log("Spectate chat",chatevent,this.messages.length)
+    return this
+  }
+
 
   override handleInput(input: Input): Controller {
     this.commonKeyHandler(input)
