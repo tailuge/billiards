e0ac1d129cd73fb98fc7eae4941a363c6480a1a3
tailuge <tailuge@gmail.com>
2025-03-24 06:59:57 +0000

    t

--- src/diagram/diagramcontainer.ts
+++ src/diagram/diagramcontainer.ts
@@ -47,7 +47,10 @@ export class DiagramContainer {
 
   onAssetsReady = () => {
     console.log(`diagram ready`)
-    this.realOverlay = new RealOverlay(document.getElementById("canvas"), this.container)
+    this.realOverlay = new RealOverlay(
+      document.getElementById("canvas"),
+      this.container
+    )
     this.breakState = JSON.parse(decodeURIComponent(this.replay))
     const replaybutton = document.getElementById("replay")! as HTMLButtonElement
     this.replayButton(replaybutton)
