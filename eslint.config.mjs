3875e77de6af371283a17d4310ce63e08db1b4f3
tailuge <tailuge@gmail.com>
2026-01-29 17:42:33 +0000

    lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,18 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import html from 'eslint-plugin-html';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ["dist/**", "node_modules/**", "webpack.config.js", "jest.config.js"],
+    ignores: [
+      "dist/*",
+      "!dist/picker.html",
+      "node_modules/**",
+      "webpack.config.js",
+      "jest.config.js"
+    ],
   },
   {
     rules: {
@@ -13,6 +20,29 @@ export default tseslint.config(
       "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
       "@typescript-eslint/no-unused-expressions": "off",
       "@typescript-eslint/no-require-imports": "off",
+    },
+  },
+  {
+    files: ["dist/picker.html"],
+    plugins: { html },
+    languageOptions: {
+      globals: {
+        document: "readonly",
+        window: "readonly",
+        console: "readonly",
+        setTimeout: "readonly",
+        requestAnimationFrame: "readonly",
+        THREE: "readonly",
+        URL: "readonly",
+        URLSearchParams: "readonly",
+        btoa: "readonly",
+        atob: "readonly",
+      },
+    },
+    rules: {
+      "no-undef": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
   {
