cd395973972f5145cac2aff8a5be1bc066cbb21c
tailuge <tailuge@gmail.com>
2026-02-19 18:19:13 +0000

    logging

--- src/container/browsercontainer.ts
+++ src/container/browsercontainer.ts
@@ -1,93 +1,93 @@
-import { Container } from "./container";
-import { ContainerConfig } from "./containerconfig";
-import { Keyboard } from "../events/keyboard";
-import { EventUtil } from "../events/eventutil";
-import { BreakEvent } from "../events/breakevent";
-import { GameEvent } from "../events/gameevent";
+import { Container } from "./container"
+import { ContainerConfig } from "./containerconfig"
+import { Keyboard } from "../events/keyboard"
+import { EventUtil } from "../events/eventutil"
+import { BreakEvent } from "../events/breakevent"
+import { GameEvent } from "../events/gameevent"
 import {
   bounceHan,
   bounceHanBlend,
   mathavenAdapter,
-} from "../model/physics/physics";
-import JSONCrush from "jsoncrush";
-import { Assets } from "../view/assets";
-import { SnookerConfig } from "../utils/snookerconfig";
-import { ThreeCushionConfig } from "../utils/threecushionconfig";
-import { Session } from "../network/client/session";
-import { MessageRelay } from "../network/client/messagerelay";
-import { NchanMessageRelay } from "../network/client/nchanmessagerelay";
-import { BotMessageRelay } from "../network/bot/botmessagerelay";
-import { ScoreReporter } from "../network/client/scorereporter";
-import { BeginEvent } from "../events/beginevent";
-import { logNetEvent } from "../utils/event-log";
+} from "../model/physics/physics"
+import JSONCrush from "jsoncrush"
+import { Assets } from "../view/assets"
+import { SnookerConfig } from "../utils/snookerconfig"
+import { ThreeCushionConfig } from "../utils/threecushionconfig"
+import { Session } from "../network/client/session"
+import { MessageRelay } from "../network/client/messagerelay"
+import { NchanMessageRelay } from "../network/client/nchanmessagerelay"
+import { BotMessageRelay } from "../network/bot/botmessagerelay"
+import { ScoreReporter } from "../network/client/scorereporter"
+import { BeginEvent } from "../events/beginevent"
+import { logNetEvent } from "../utils/event-log"
 
 /**
  * Integrate game container into HTML page
  */
 export class BrowserContainer {
-  container: Container;
-  canvas3d;
-  tableId;
-  clientId;
-  wss;
-  ruletype;
-  playername: string;
-  replay: string | null;
-  messageRelay: MessageRelay | null = null;
+  container: Container
+  canvas3d
+  tableId
+  clientId
+  wss
+  ruletype
+  playername: string
+  replay: string | null
+  messageRelay: MessageRelay | null = null
   breakState: {
-    init: any;
-    shots: any[];
-    now: number;
-    score: number;
-    players?: { player1: string; player2: string };
+    init: any
+    shots: any[]
+    now: number
+    score: number
+    players?: { player1: string; player2: string }
   } = {
     init: null,
     shots: [],
     now: 0,
     score: 0,
-  };
-  cushionModel;
-  spectator;
-  first;
-  assets: Assets;
-  now;
-  botMode: boolean = false;
-  readonly botDelay: number = 500;
+  }
+  cushionModel
+  spectator
+  first
+  assets: Assets
+  now
+  botMode: boolean = false
+  readonly botDelay: number = 500
   constructor(canvas3d, params) {
-    this.now = Date.now();
-    this.playername = params.get("name") ?? params.get("playername") ?? "Anon";
-    this.tableId = params.get("tableId") ?? "default";
-    this.clientId = params.get("clientId") ?? "default";
-    this.replay = params.get("state");
-    this.ruletype = params.get("ruletype") ?? "nineball";
-    this.wss = params.get("websocketserver");
-    this.canvas3d = canvas3d;
-    this.cushionModel = this.cushion(params.get("cushionModel"));
-    this.spectator = params.has("spectator");
-    this.first = params.has("first");
-    this.botMode = params.has("bot");
-    SnookerConfig.reds = parseInt(params.get("reds") ?? "15") || 15;
-    ThreeCushionConfig.raceTo = parseInt(params.get("raceTo") ?? "2") || 2;
+    this.now = Date.now()
+    this.playername = params.get("name") ?? params.get("playername") ?? "Anon"
+    this.tableId = params.get("tableId") ?? "default"
+    this.clientId = params.get("clientId") ?? "default"
+    this.replay = params.get("state")
+    this.ruletype = params.get("ruletype") ?? "nineball"
+    this.wss = params.get("websocketserver")
+    this.canvas3d = canvas3d
+    this.cushionModel = this.cushion(params.get("cushionModel"))
+    this.spectator = params.has("spectator")
+    this.first = params.has("first")
+    this.botMode = params.has("bot")
+    SnookerConfig.reds = parseInt(params.get("reds") ?? "15") || 15
+    ThreeCushionConfig.raceTo = parseInt(params.get("raceTo") ?? "2") || 2
     console.log(
-      `clientId: ${this.clientId} playername: ${this.playername} tableId: ${this.tableId} spectator: ${this.spectator} botMode: ${this.botMode}`,
-    );
+      `clientId: ${this.clientId} playername: ${this.playername} tableId: ${this.tableId} spectator: ${this.spectator} botMode: ${this.botMode}`
+    )
     Session.init(
       this.clientId,
       this.playername,
       this.tableId,
       this.spectator,
-      this.botMode,
-    );
+      this.botMode
+    )
   }
 
   cushion(model) {
     switch (model) {
       case "bounceHan":
-        return bounceHan;
+        return bounceHan
       case "bounceHanBlend":
-        return bounceHanBlend;
+        return bounceHanBlend
       default:
-        return mathavenAdapter;
+        return mathavenAdapter
     }
   }
 
@@ -101,118 +101,118 @@ export class BrowserContainer {
       id: this.playername,
       relay: this.messageRelay,
       scoreReporter: scoreReporter,
-    };
-    return new Container(config);
+    }
+    return new Container(config)
   }
 
   start() {
-    this.assets = new Assets(this.ruletype);
+    this.assets = new Assets(this.ruletype)
     this.assets.loadFromWeb(() => {
-      this.onAssetsReady();
-    });
+      this.onAssetsReady()
+    })
   }
 
   onAssetsReady() {
-    console.log(`${this.playername} assets ready`);
-    const scoreReporter = new ScoreReporter();
+    console.log(`${this.playername} assets ready`)
+    const scoreReporter = new ScoreReporter()
 
     if (this.botMode) {
-      this.messageRelay = new BotMessageRelay();
-      this.container = this.createContainer(scoreReporter);
-      this.container.isSinglePlayer = false;
+      this.messageRelay = new BotMessageRelay()
+      this.container = this.createContainer(scoreReporter)
+      this.container.isSinglePlayer = false
       this.messageRelay.subscribe(this.tableId, (e) => {
-        this.netEvent(e);
-      });
+        this.netEvent(e)
+      })
     } else {
-      this.messageRelay = new NchanMessageRelay();
-      this.container = this.createContainer(scoreReporter);
+      this.messageRelay = new NchanMessageRelay()
+      this.container = this.createContainer(scoreReporter)
     }
 
     this.container.broadcast = (e) => {
-      this.broadcast(e);
-    };
-    this.container.table.cushionModel = this.cushionModel;
-    this.setReplayLink();
+      this.broadcast(e)
+    }
+    this.container.table.cushionModel = this.cushionModel
+    this.setReplayLink()
 
     if (this.spectator) {
-      this.container.eventQueue.push(new BeginEvent());
-      this.container.animate(performance.now());
-      return;
+      this.container.eventQueue.push(new BeginEvent())
+      this.container.animate(performance.now())
+      return
     }
 
     if (this.wss) {
-      this.container.isSinglePlayer = false;
+      this.container.isSinglePlayer = false
       this.messageRelay.subscribe(this.tableId, (e) => {
-        this.netEvent(e);
-      });
+        this.netEvent(e)
+      })
       if (!this.first && !this.spectator) {
-        this.broadcast(new BeginEvent());
+        this.broadcast(new BeginEvent())
       }
     }
 
     if (this.replay) {
-      this.startReplay(this.replay);
+      this.startReplay(this.replay)
     } else if (this.container.isSinglePlayer) {
-      this.container.eventQueue.push(new BreakEvent());
+      this.container.eventQueue.push(new BreakEvent())
     }
 
     // trigger animation loops
-    this.container.animate(performance.now());
+    this.container.animate(performance.now())
   }
 
   netEvent(e: string) {
-    const event = EventUtil.fromSerialised(e);
-    logNetEvent(this.playername, event, "receive");
+    const event = EventUtil.fromSerialised(e)
+    logNetEvent(this.playername, event, "receive")
     if (event.clientId !== Session.getInstance().clientId) {
       if (event.playername) {
-        Session.getInstance().opponentName = event.playername;
+        Session.getInstance().opponentName = event.playername
       }
-      this.container.eventQueue.push(event);
+      this.container.eventQueue.push(event)
     } else {
-      console.log("Ignoring own event");
+      console.log("Ignoring own event")
     }
   }
 
   broadcast(event: GameEvent) {
     if (this.messageRelay) {
-      event.clientId = Session.getInstance().clientId;
-      event.playername = Session.getInstance().playername;
-      logNetEvent(this.playername, event, "broadcast");
-      this.messageRelay.publish(this.tableId, EventUtil.serialise(event));
+      event.clientId = Session.getInstance().clientId
+      event.playername = Session.getInstance().playername
+      logNetEvent(this.playername, event, "broadcast")
+      this.messageRelay.publish(this.tableId, EventUtil.serialise(event))
     }
   }
 
   setReplayLink() {
-    const url = window.location.href.split("?")[0];
-    const prefix = `${url}?ruletype=${this.ruletype}&state=`;
-    this.container.linkFormatter.replayUrl = prefix;
+    const url = window.location.href.split("?")[0]
+    const prefix = `${url}?ruletype=${this.ruletype}&state=`
+    this.container.linkFormatter.replayUrl = prefix
   }
 
   startReplay(replay) {
-    this.breakState = this.parse(replay);
+    this.breakState = this.parse(replay)
     if (this.breakState.players) {
-      const session = Session.getInstance();
-      session.playername = this.breakState.players.player1;
-      session.opponentName = this.breakState.players.player2;
+      const session = Session.getInstance()
+      session.playername = this.breakState.players.player1
+      session.opponentName = this.breakState.players.player2
     }
     const breakEvent = new BreakEvent(
       this.breakState.init,
-      this.breakState.shots,
-    );
-    this.container.eventQueue.push(breakEvent);
+      this.breakState.shots
+    )
+    this.container.eventQueue.push(breakEvent)
   }
 
   parse(s) {
     try {
-      return JSON.parse(s);
+      return JSON.parse(s)
     } catch {
-      return JSON.parse(JSONCrush.uncrush(s));
+      return JSON.parse(JSONCrush.uncrush(s))
     }
   }
 
   offerUpload() {
     this.container.chat.showMessage(
-      `<a class="pill" target="_blank" href="https://scoreboard-tailuge.vercel.app/hiscore.html${location.search}"> upload high score 🏆</a`,
-    );
+      `<a class="pill" target="_blank" href="https://scoreboard-tailuge.vercel.app/hiscore.html${location.search}"> upload high score 🏆</a`
+    )
   }
 }
--- src/network/bot/botmessagerelay.ts
+++ src/network/bot/botmessagerelay.ts
@@ -1,59 +1,71 @@
-import { MessageRelay } from "../client/messagerelay";
-import { BotDebugOverlay } from "./overlay";
-import { BeginEvent } from "../../events/beginevent";
-import { EventUtil } from "../../events/eventutil";
+import { MessageRelay } from "../client/messagerelay"
+import { BotDebugOverlay } from "./overlay"
+import { BeginEvent } from "../../events/beginevent"
+import { EventUtil } from "../../events/eventutil"
+import { EventType } from "../../events/eventtype"
 
 export class BotMessageRelay implements MessageRelay {
-  private messageQueue: string[] = [];
-  private callback: ((message: string) => void) | null = null;
-  private logs: BotDebugOverlay;
+  private messageQueue: string[] = []
+  private callback: ((message: string) => void) | null = null
+  private logs: BotDebugOverlay
 
   constructor() {
-    this.logs = new BotDebugOverlay();
+    this.logs = new BotDebugOverlay()
+  }
+
+  private isAimEvent(message: string): boolean {
+    try {
+      const event = EventUtil.fromSerialised(message)
+      return event.type === EventType.AIM
+    } catch {
+      return false
+    }
   }
 
   subscribe(
     channel: string,
     callback: (message: string) => void,
-    prefix?: string,
+    prefix?: string
   ): void {
-    this.callback = callback;
-    this.logs.info(`BotMessageRelay subscribed to ${prefix || ""}${channel}`);
+    this.callback = callback
+    this.logs.info(`BotMessageRelay subscribed to ${prefix || ""}${channel}`)
 
     while (this.messageQueue.length > 0) {
-      const message = this.messageQueue.shift();
+      const message = this.messageQueue.shift()
       if (message && this.callback) {
-        this.callback(message);
+        this.callback(message)
       }
     }
 
     setTimeout(() => {
-      const beginEvent = EventUtil.serialise(new BeginEvent());
-      this.logs.incoming(`Bot joining: BeginEvent`);
-      callback(beginEvent);
-    }, 0);
+      const beginEvent = EventUtil.serialise(new BeginEvent())
+      this.logs.incoming(`Bot joining: BeginEvent`)
+      callback(beginEvent)
+    }, 0)
   }
 
   publish(channel: string, message: string, prefix?: string): void {
-    this.logs.outgoing(
-      `Publish to ${prefix || ""}${channel}: ${message.substring(0, 50)}...`,
-    );
+    if (!this.isAimEvent(message)) {
+      this.logs.outgoing(`Publish to ${prefix || ""}${channel}: ${message}`)
+    }
 
-    this.messageQueue.push(message);
+    this.messageQueue.push(message)
 
     if (this.callback) {
       setTimeout(() => {
-        this.callback?.(message);
-      }, 0);
+        this.callback?.(message)
+      }, 0)
     }
   }
 
   async getOnlineCount(): Promise<number | null> {
-    return 2;
+    return 2
   }
 
   queueMessage(message: string): void {
-    this.messageQueue.push(message);
-    this.logs.incoming(`Queued: ${message.substring(0, 50)}...`);
+    this.messageQueue.push(message)
+    if (!this.isAimEvent(message)) {
+      this.logs.incoming(`Queued: ${message}`)
+    }
   }
 }
