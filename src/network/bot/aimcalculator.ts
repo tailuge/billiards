45fff4f3f895b8cc0e97bfbeb306dd1abcaee2ec
tailuge <tailuge@gmail.com>
2026-02-22 09:25:30 +0000

    respot

--- src/network/bot/aimcalculator.ts
+++ src/network/bot/aimcalculator.ts
@@ -1,81 +1,85 @@
-import { Vector3 } from "three";
-import { HitEvent } from "../../events/hitevent";
-import { Table } from "../../model/table";
-import { R } from "../../model/physics/constants";
-import { atan2 } from "../../utils/utils";
-import { Pocket } from "../../model/physics/pocket";
+import { Vector3 } from "three"
+import { HitEvent } from "../../events/hitevent"
+import { Table } from "../../model/table"
+import { R } from "../../model/physics/constants"
+import { atan2 } from "../../utils/utils"
+import { Pocket } from "../../model/physics/pocket"
 
 export class AimCalculator {
-  private readonly ballRadius: number;
+  private readonly ballRadius: number
 
   constructor() {
-    this.ballRadius = R;
+    this.ballRadius = R
   }
 
   public getAimPoint(
     cuePos: Vector3,
     targetPos: Vector3,
-    pockets: Vector3[],
+    pockets: Vector3[]
   ): Vector3 | null {
-    const bestPocket = this.findBestPocket(cuePos, targetPos, pockets);
+    const bestPocket = this.findBestPocket(cuePos, targetPos, pockets)
 
-    if (!bestPocket) return null;
-    return this.calculateGhostBallPos(targetPos, bestPocket);
+    if (!bestPocket) return null
+    return this.calculateGhostBallPos(targetPos, bestPocket)
   }
 
   public extractPocketPositions(pockets: Pocket[]): Vector3[] {
-    return pockets.map((pocket) => pocket.pos.clone().multiplyScalar(0.95));
+    return pockets.map((pocket) => pocket.pos.clone().multiplyScalar(0.95))
   }
 
-  generateRandomShot(table: Table, noise: number, targetPos?: Vector3): HitEvent {
-    const cueball = table.cueball;
-    const aim = table.cue.aim;
+  generateRandomShot(
+    table: Table,
+    noise: number,
+    targetPos?: Vector3
+  ): HitEvent {
+    const cueball = table.cueball
+    const aim = table.cue.aim
 
     if (!targetPos) {
       targetPos = new Vector3().random()
     }
 
-    const lineTo = targetPos.clone().sub(cueball.pos);
-    aim.angle = atan2(lineTo.y, lineTo.x) + (Math.random() - 0.5) * noise;
-    aim.power = 80 * R;
-    aim.offset = new Vector3(0, (Math.random() - 0.5) * 0.6);
-    return new HitEvent(table.serialise());
+    const lineTo = targetPos.clone().sub(cueball.pos)
+    aim.angle = atan2(lineTo.y, lineTo.x) + (Math.random() - 0.5) * noise
+    aim.power = 80 * R
+    aim.offset = new Vector3(0, (Math.random() - 0.5) * 0.6)
+    return new HitEvent(table.serialise())
   }
 
   private findBestPocket(
     cuePos: Vector3,
     targetPos: Vector3,
-    pockets: Vector3[],
+    pockets: Vector3[]
   ): Vector3 | undefined {
     return pockets
       .filter((pocket) => this.isPocketAhead(cuePos, targetPos, pocket))
       .sort(
-        (a, b) => this.distanceSq(targetPos, a) - this.distanceSq(targetPos, b),
-      )[0];
+        (a, b) => this.distanceSq(targetPos, a) - this.distanceSq(targetPos, b)
+      )[0]
   }
 
   private isPocketAhead(
     cuePos: Vector3,
     targetPos: Vector3,
-    pocket: Vector3,
+    pocket: Vector3
   ): boolean {
-    const shotLine = this.getDirectionVector(cuePos, targetPos);
-    const pocketLine = this.getDirectionVector(targetPos, pocket);
-    return shotLine.dot(pocketLine) > 0;
+    const shotLine = this.getDirectionVector(cuePos, targetPos)
+    const pocketLine = this.getDirectionVector(targetPos, pocket)
+    return shotLine.dot(pocketLine) > 0
   }
 
   private calculateGhostBallPos(targetPos: Vector3, pocket: Vector3): Vector3 {
-    const incidentVector = this.getDirectionVector(pocket, targetPos);
+    const incidentVector = this.getDirectionVector(pocket, targetPos)
     return targetPos
       .clone()
-      .add(incidentVector.multiplyScalar(this.ballRadius * 2.01));
+      .add(incidentVector.multiplyScalar(this.ballRadius * 2.01))
   }
 
   private getDirectionVector(from: Vector3, to: Vector3): Vector3 {
-    return new Vector3().subVectors(to, from).normalize();
+    return new Vector3().subVectors(to, from).normalize()
   }
 
   private distanceSq(v1: Vector3, v2: Vector3): number {
-    return v1.distanceToSquared(v2);
+    return v1.distanceToSquared(v2)
   }
 }
--- src/network/bot/eventhandler.ts
+++ src/network/bot/eventhandler.ts
@@ -1,47 +1,49 @@
-import { GameEvent } from "../../events/gameevent";
-import { Logger } from "./logger";
-import { Container } from "../../container/container";
-import { EventType } from "../../events/eventtype";
-import { HitEvent } from "../../events/hitevent";
-import { AimCalculator } from "./aimcalculator";
-import { PocketGeometry } from "../../view/pocketgeometry";
-import { StartAimEvent } from "../../events/startaimevent";
-import { Outcome } from "../../model/outcome";
-import { NineBall } from "../../controller/rules/nineball";
-import { PlaceBallEvent } from "../../events/placeballevent";
-import { WatchEvent } from "../../events/watchevent";
-import { EventUtil } from "../../events/eventutil";
+import { GameEvent } from "../../events/gameevent"
+import { Logger } from "./logger"
+import { Container } from "../../container/container"
+import { EventType } from "../../events/eventtype"
+import { HitEvent } from "../../events/hitevent"
+import { AimCalculator } from "./aimcalculator"
+import { PocketGeometry } from "../../view/pocketgeometry"
+import { StartAimEvent } from "../../events/startaimevent"
+import { Outcome } from "../../model/outcome"
+import { NineBall } from "../../controller/rules/nineball"
+import { PlaceBallEvent } from "../../events/placeballevent"
+import { WatchEvent } from "../../events/watchevent"
+import { EventUtil } from "../../events/eventutil"
+import { RerackEvent } from "../../events/rerackevent"
+import { Respot } from "../../utils/respot"
 
 export class BotEventHandler {
-  private logs: Logger;
-  private container: Container;
-  private publishToPlayer: (event: GameEvent) => void;
-  protected enqueueMessage: (message: string) => void;
-  private readonly calculator: AimCalculator;
+  private logs: Logger
+  private container: Container
+  private publishToPlayer: (event: GameEvent) => void
+  protected enqueueMessage: (message: string) => void
+  private readonly calculator: AimCalculator
 
   constructor(
     logs: Logger,
     container: Container,
     publishToPlayer: (event: GameEvent) => void,
-    enqueueMessage: (message: string) => void,
+    enqueueMessage: (message: string) => void
   ) {
-    this.logs = logs;
-    this.container = container;
-    this.publishToPlayer = publishToPlayer;
-    this.enqueueMessage = enqueueMessage;
-    this.calculator = new AimCalculator();
+    this.logs = logs
+    this.container = container
+    this.publishToPlayer = publishToPlayer
+    this.enqueueMessage = enqueueMessage
+    this.calculator = new AimCalculator()
   }
 
   handle(event): void {
-    this.logs.info(`Bot handling event: ${event.type}`);
+    this.logs.info(`Bot handling event: ${event.type}`)
     if (event.type === EventType.STARTAIM) {
-      this.handleStartAim();
+      this.handleStartAim()
     }
     if (event.type === EventType.PLACEBALL) {
-      this.handlePlaceBall(event);
+      this.handlePlaceBall(event)
     }
     if (event.type === EventType.BEGIN) {
-      this.handleStationary();
+      this.handleStationary()
     }
   }
 
@@ -49,85 +51,97 @@ export class BotEventHandler {
     // The balls have finished rolling after a shot, now apply rules to find out
     // if turn continues or switches maybe with a foul and a placeball
     // gameover is also an option
-    const outcome = this.container.table.outcome;
+    const outcome = this.container.table.outcome
     if (this.container.rules.isEndOfGame(outcome)) {
       // bot has won, notify player
     }
 
-    const foulReason = NineBall.foulReason(this.container.table, outcome);
+    const foulReason = NineBall.foulReason(this.container.table, outcome)
     if (foulReason) {
-      // bot has fouled, notify player
       this.container.notify({
         type: "Foul",
         title: "FOUL",
         subtext: foulReason,
         extra: "Ball in hand",
-      });
+      })
 
-      // in nineball it is ball in hand
-      const cueball = this.container.table.cueball;
+      const cueball = this.container.table.cueball
       const startPos = cueball.onTable()
         ? cueball.pos.clone()
-        : this.container.rules.placeBall();
-      const placeBallEvent = new PlaceBallEvent(startPos, undefined, true);
-      this.publishToPlayer(placeBallEvent);
+        : this.container.rules.placeBall()
+
+      const nineBall = this.container.table.balls[9]
+      const nineBallPotted = Outcome.pots(outcome).includes(nineBall)
+      let respot
+      if (nineBallPotted) {
+        Respot.nineBall(this.container.table)
+        respot = RerackEvent.fromJson({
+          balls: [nineBall.serialise()],
+        })
+      }
+      const placeBallEvent = new PlaceBallEvent(startPos, respot, true)
+      this.publishToPlayer(placeBallEvent)
+      return
     }
 
     if (Outcome.potCount(outcome) > 0) {
       // pot success, send watch event to other player
-      this.publishToPlayer(new WatchEvent(this.container.table.serialise()));
+      this.publishToPlayer(new WatchEvent(this.container.table.serialise()))
       // this player has to take another shot.
-      this.enqueueMessage(EventUtil.serialise(new StartAimEvent()));
-      return;
+      this.enqueueMessage(EventUtil.serialise(new StartAimEvent()))
+      return
     }
 
     // switch to players turn
-    this.publishToPlayer(new StartAimEvent());
+    this.publishToPlayer(new StartAimEvent())
   }
 
   private handleStartAim(): void {
-    const hitEvent = this.generateCalculatedShot();
-    this.publishToPlayer(hitEvent);
+    const hitEvent = this.aimAndHit()
+    this.publishToPlayer(hitEvent)
   }
 
   private handlePlaceBall(event: PlaceBallEvent): void {
-    const table = this.container.table;
+    const table = this.container.table
 
     if (event.respot) {
-      const ball = table.balls.find((b) => b.id === event.respot!.id);
+      const ball = table.balls.find((b) => b.id === event.respot!.id)
       if (ball) {
-        ball.pos.copy(event.respot!.pos);
-        ball.setStationary();
+        ball.pos.copy(event.respot!.pos)
+        ball.setStationary()
       }
     }
 
-    const cueball = table.cueball;
+    const cueball = table.cueball
     cueball.pos.copy(
-      event.useStartPos ? event.pos : this.container.rules.placeBall(),
-    );
-    cueball.setStationary();
-    const hitEvent = this.generateCalculatedShot();
-    this.publishToPlayer(hitEvent);
+      event.useStartPos ? event.pos : this.container.rules.placeBall()
+    )
+    cueball.setStationary()
+    const hitEvent = this.aimAndHit()
+    this.publishToPlayer(hitEvent)
   }
 
-  
-  private generateCalculatedShot(): HitEvent {
-    const table = this.container.table;
-    const cueball = table.cueball;
-    const targetBall = this.container.rules.nextCandidateBall();
+  private aimAndHit(): HitEvent {
+    const table = this.container.table
+    const cueball = table.cueball
+    const targetBall = this.container.rules.nextCandidateBall()
 
     if (!targetBall) {
-      return this.calculator.generateRandomShot(table, 0.1, targetBall?.pos);
+      return this.calculator.generateRandomShot(table, 0.1, targetBall?.pos)
     }
 
     const pockets = this.calculator.extractPocketPositions(
-      PocketGeometry.pocketCenters,
-    );
+      PocketGeometry.pocketCenters
+    )
     const aimPoint = this.calculator.getAimPoint(
       cueball.pos,
       targetBall.pos,
-      pockets,
-    );
-    return this.calculator.generateRandomShot(table, 0.01, aimPoint ?? undefined);
+      pockets
+    )
+    return this.calculator.generateRandomShot(
+      table,
+      0.01,
+      aimPoint ?? undefined
+    )
   }
 }
