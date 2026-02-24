98e895a0ee8001fb6f1fa4e4ae19e79749b7dc78
tailuge <tailuge@gmail.com>
2026-02-24 06:08:49 +0000

    deps

--- test/mocks/inmemorymessagerelay.ts
+++ test/mocks/inmemorymessagerelay.ts
@@ -1,10 +1,8 @@
 import { MessageRelay } from "../../src/network/client/messagerelay"
 
 export class InMemoryMessageRelay implements MessageRelay {
-  private readonly subscriptions: Map<
-    string,
-    Set<(message: string) => void>
-  > = new Map()
+  private readonly subscriptions: Map<string, Set<(message: string) => void>> =
+    new Map()
 
   subscribe(
     channel: string,
