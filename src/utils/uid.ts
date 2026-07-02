f39774a242861557bb8544873931cf21afffd7c6
tailuge <tailuge@gmail.com>
2026-07-02 06:46:33 +0100

    ids

--- src/utils/uid.ts
+++ src/utils/uid.ts
@@ -1,7 +1,19 @@
 import { VERSION } from "./version"
 
-export function getUID() {
-  return `xxxx_${VERSION.substring(0, 6)}`.replace(/x/g, () =>
+function generateRandomHex4(): string {
+  return "xxxx".replace(/x/g, () =>
     Math.floor(Math.random() * 16).toString(16)
   )
+}
+
+function getAnonId(): string {
+  const stored = globalThis.localStorage?.getItem("anonId")
+  if (stored) return stored
+  const id = generateRandomHex4()
+  globalThis.localStorage?.setItem("anonId", id)
+  return id
+}
+
+export function getUID() {
+  return `${getAnonId()}_${VERSION.substring(0, 6)}`
 }
