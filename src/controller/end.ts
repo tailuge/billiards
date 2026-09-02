b8a19a753cede302e802b69f9851b979dc820515
tailuge <tailuge@gmail.com>
2026-09-02 13:08:49 +0100

    result

--- src/controller/end.ts
+++ src/controller/end.ts
@@ -7,6 +7,7 @@ import { Session } from "../network/client/session"
 import { ResumeStore } from "../utils/resumestore"
 import { BreakEvent } from "../events/breakevent"
 import { Replay } from "./replay"
+import { getUID } from "../utils/uid"
 
 async function submitResults(
   container: Container,
@@ -21,9 +22,14 @@ async function submitResults(
     return
   }
 
+  const challengeId =
+    Session.isBotMode() && session.tableId === "default"
+      ? `G_${getUID()}`
+      : session.tableId
+
   await scoreReporter.submitTournamentResult(
     session.tournamentId,
-    session.tableId,
+    challengeId,
     result.winnerId,
     result.loserId
   )
