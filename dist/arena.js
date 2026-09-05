a448f9cd03a9e9c5a720568992651a5a8830aec9
tailuge <tailuge@gmail.com>
2026-09-05 06:31:33 +0100

    deps

--- dist/arena.js
+++ dist/arena.js
@@ -315,6 +315,7 @@
     online-panel    { grid-area: 1 / 2 / 2 / 3; }
     online-panel.panel { overflow-y: auto; max-height: calc(100vh - 6rem); align-self: stretch; }
     .arenas-row     { grid-area: 2 / 1 / 3 / 3; }
+    .arenas-row.arena-details { padding: 2px; }
     .info-row       { grid-area: 3 / 1 / 4 / 3; }
 
     main.has-sidebar {
