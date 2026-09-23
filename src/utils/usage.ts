fe0b6f0463ac576c19cb32a17e738bf783763411
tailuge <tailuge@gmail.com>
2026-09-23 07:41:17 +0100

    usage

--- src/utils/usage.ts
+++ src/utils/usage.ts
@@ -7,7 +7,7 @@ export function logusage() {
     return
   }
 
-  const url = `https://scoreboard-tailuge.vercel.app/api/usage/game`
+  const url = `https://billiards-network.onrender.com/api/usage/game`
 
   fetch(url, {
     method: "PUT",
